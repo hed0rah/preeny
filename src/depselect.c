58bf5ad4b69c3f4b953a35bd44d7469f9ecc9da3
Alexandre DOYEN <alexandre.doyen@cea.fr>
2025-05-19 09:45:26 +0200

    PREENY_DEPSELECT_RETURNED_FD envuironment variable to choose the returned FD

--- src/depselect.c
+++ src/depselect.c
@@ -5,29 +5,43 @@
 #include <dlfcn.h>
 #include <sys/select.h>
 #include <time.h>
+#include <stdlib.h>
 
 int (*real_pselect)(int, fd_set*, fd_set*, fd_set*, const struct timespec*, const sigset_t*);
 int (*real_pselect6)(int, fd_set*, fd_set*, fd_set*, const struct timespec*, const sigset_t*);
 int (*real_select)(int, fd_set*, fd_set*, fd_set*, struct timeval*);
+char* returned_fd_env;
+int returned_fd;
 
 __attribute__((constructor)) void __depselect_init(void)
 {
     real_pselect = dlsym(RTLD_NEXT, "pselect");
     real_pselect6 = dlsym(RTLD_NEXT, "pselect6");
     real_select = dlsym(RTLD_NEXT, "select");
+
+    returned_fd_env = getenv("PREENY_DEPSELECT_RETURNED_FD");
+
+    if (returned_fd_env == NULL)
+    {
+        returned_fd = 1;
+    }
+    else
+    {
+        returned_fd = atoi(returned_fd_env);
+    }
 }
 
 int pselect(int nfds, fd_set* restrict readfds, fd_set* restrict writefds, fd_set* restrict exceptfds, const struct timespec* restrict timeout, const __sigset_t* restrict sigmask)
 {
-    return 1;
+    return returned_fd;
 }
 
 int pselect6(int nfds, fd_set* restrict readfds, fd_set* restrict writefds, fd_set* restrict exceptfds, const struct timespec* restrict timeout, const __sigset_t* restrict sigmask)
 {
-    return 1;
+    return returned_fd;
 }
 
 int select(int nfds, fd_set *restrict readfds, fd_set *restrict writefds, fd_set *restrict exceptfds, struct timeval *restrict timeout)
 {
-    return 1;
+    return returned_fd;
 }
