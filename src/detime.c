d1a65c37c00dccf5bd5764e0a09e611e71e424da
hed0rah <18272116+hed0rah@users.noreply.github.com>
2026-04-07 21:44:53 -0400

    detime.c - added clock_gettime() hook, nowrite.c - added openat() hook, CMakeLists.txt - made patch.so optional via find_library

--- src/detime.c
+++ src/detime.c
@@ -9,16 +9,41 @@
 
 #include "logging.h"
 
+// frozen time values, read once from env
+static time_t frozen_sec = 0;
+static long frozen_nsec = 0;
+static int frozen_init = 0;
+
+static void detime_init(void)
+{
+	if (frozen_init) return;
+	char *sec_str = getenv("TIME");
+	char *nsec_str = getenv("TIME_NSEC");
+	frozen_sec = sec_str ? atol(sec_str) : 0;
+	frozen_nsec = nsec_str ? atol(nsec_str) : 0;
+	frozen_init = 1;
+}
+
 time_t time(time_t *res)
 {
-	char *time_str = getenv("TIME");
-	time_t time = time_str ? atoi(time_str) : 0;
-	if(res != NULL) {
-		*res = time;
+	detime_init();
+	if (res != NULL)
+		*res = frozen_sec;
+
+	preeny_debug("time frozen at %ld\n", (long)frozen_sec);
+	return frozen_sec;
+}
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp)
+{
+	detime_init();
+	if (tp != NULL) {
+		tp->tv_sec = frozen_sec;
+		tp->tv_nsec = frozen_nsec;
 	}
 
-	preeny_debug("time frozen at %d\n", time);
-	return time;
+	preeny_debug("clock_gettime(%d) frozen at %ld.%09ld\n", clk_id, (long)frozen_sec, frozen_nsec);
+	return 0;
 }
 
 #ifdef __unix__
@@ -28,15 +53,12 @@ int gettimeofday(struct timeval *tv, void *__restrict tz)
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 #endif
 {
-	
-	char *sec_str = getenv("TV_SEC");
-	char *usec_str = getenv("TV_USEC");
-	tv->tv_sec = sec_str ? atoi(sec_str) : 0;
-	tv->tv_usec = usec_str ? atoi(usec_str) : 0;
+	detime_init();
+	tv->tv_sec = frozen_sec;
+	tv->tv_usec = frozen_nsec / 1000;
 
-	preeny_debug("gettimeofday frozen at %ld.%d\n", tv->tv_sec, tv->tv_usec);
-	
-	
+	preeny_debug("gettimeofday frozen at %ld.%06ld\n", (long)tv->tv_sec, (long)tv->tv_usec);
+
 #if !defined(__GLIBC__) || __GLIBC_PREREQ(2, 31)
 #else
 	if (tz != NULL) {
@@ -44,8 +66,6 @@ int gettimeofday(struct timeval *tv, struct timezone *tz)
 		char *dst_str = getenv("TZ_DSTTIME");
 		tz->tz_minuteswest = minwest_str ? atoi(minwest_str) : 0;
 		tz->tz_dsttime = dst_str ? atoi(dst_str) : 0;
-
-		preeny_debug("gettimeofday tz frozen at -%d minm, DST: %d\n", tz->tz_minuteswest, tz->tz_dsttime);
 	}
 #endif
 
--- src/nowrite.c
+++ src/nowrite.c
@@ -4,19 +4,40 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <dlfcn.h>
+#include <stdarg.h>
+
+#include "logging.h"
+
+// write-intent flags to strip
+#define WRITE_FLAGS (O_WRONLY|O_RDWR|O_CREAT|O_EXCL|O_TRUNC|O_APPEND)
 
 int (*original_open)(const char *pathname, int flags, ...);
+int (*original_openat)(int dirfd, const char *pathname, int flags, ...);
 
 __attribute__((constructor)) void preeny_nowrite()
 {
 	original_open = dlsym(RTLD_NEXT, "open");
+	original_openat = dlsym(RTLD_NEXT, "openat");
+}
+
+static int sanitize_flags(int flags)
+{
+	// only strip flags that imply write intent
+	// preserve O_CLOEXEC, O_DIRECTORY, O_NOFOLLOW, O_NOCTTY, O_NONBLOCK, etc.
+	flags &= ~WRITE_FLAGS;
+	flags |= O_RDONLY;
+	return flags;
 }
 
 int open(const char *pathname, int flags, ...) {
-	// Strip write-related flags & force readonly flag
-	flags &= ~(O_WRONLY|O_RDWR|O_CLOEXEC|O_CREAT|O_DIRECTORY|O_EXCL|O_NOCTTY|O_NOFOLLOW|O_TMPFILE|O_TRUNC);
-	flags |= O_RDONLY;
+	int clean = sanitize_flags(flags);
+	preeny_info("nowrite: open(\"%s\", %#x -> %#x)\n", pathname, flags, clean);
+	return original_open(pathname, clean);
+}
 
-	return original_open(pathname, flags);
+int openat(int dirfd, const char *pathname, int flags, ...) {
+	int clean = sanitize_flags(flags);
+	preeny_info("nowrite: openat(%d, \"%s\", %#x -> %#x)\n", dirfd, pathname, flags, clean);
+	return original_openat(dirfd, pathname, clean);
 }
 
